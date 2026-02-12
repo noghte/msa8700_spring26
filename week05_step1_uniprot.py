908023e0ffd60dfe36c587d7bfdd3c039157f952
Saber Soleymani <postorder@gmail.com>
2026-02-11 20:39:21 -0500

    99 kinases

--- week05_step1_uniprot.py
+++ week05_step1_uniprot.py
@@ -9,20 +9,26 @@ def extract_protein_function(data):
     return "N/A"
 
 df = pd.read_csv("kinase_classification.csv")
-uniprot_ids = df["uniprot"].dropna().unique()[:40]
-print(uniprot_ids)
+uniprot_ids = df["uniprot"].dropna().unique()[:100]
 
 functions = []
 names = []
-
+index = 0
 for uniprot_id in uniprot_ids:
+    index +=1
     url = f"https://rest.uniprot.org/uniprotkb/{uniprot_id}.json"
     r = requests.get(url)
     data = r.json()
-    protein_name = data['proteinDescription']['recommendedName']['fullName']['value']
+    if data["entryType"] == "Inactive":
+        protein_name = "N/A"
+        protein_function = "N/A"
+    else:
+        protein_name = data['proteinDescription']['recommendedName']['fullName']['value']
+        protein_function = extract_protein_function(data)
+    
     names.append(protein_name)
-    protein_function = extract_protein_function(data)
     functions.append(protein_function)
+    print(f"Index: {index}, Uniprot: {uniprot_id}")
 
 df_output = pd.DataFrame({"uniprot": uniprot_ids, "name": names, "functions": functions})
 df_output.to_csv("kinases.csv", index=False)
