beb259e5fb3e46ab6c1124c4ed8c940b5ff64168
Saber Soleymani <postorder@gmail.com>
2026-02-25 21:23:47 -0500

    travily tool added

--- week07_agent_example.py
+++ week07_agent_example.py
@@ -3,6 +3,10 @@ from langgraph.prebuilt import create_react_agent
 from langchain_ollama import ChatOllama
 from langchain_core.tools import tool
 from langchain_core.messages import SystemMessage, HumanMessage
+from tavily import TavilyClient
+from dotenv import load_dotenv
+load_dotenv()
+
 # import requests
 
 def load_reviews():
@@ -18,7 +22,7 @@ def extract_reviews_by_person(person_name, max_reviews=5):
 
     Args:
         person_name (str): The username of the person who write the review
-        max_reviews (int): Maximum number of reviews. Use the default value 5.
+        max_reviews (int, optional): Maximum number of reviews. Use the default value 5.
     """
     df = load_reviews()
     name = person_name.strip().lower()
@@ -28,6 +32,22 @@ def extract_reviews_by_person(person_name, max_reviews=5):
     
     return "\n\n".join(reviews[:max_reviews])
 
+@tool
+def web_search_product(product_query, max_results=10):
+    """
+    Search the web for the product (prefer Amazon links) using Tavily.
+
+    Args:
+        product_query (string): The product name user is searching for.
+        max_results (int, optional): Defaults to 10.
+    """
+    travily_client = TavilyClient() # api_key="tvly-YOUR_API_KEY"
+    response = travily_client.search(query=product_query, search_depth="basic",max_results=max_results)
+    for result in response["results"]:
+        if "amazon.com" in result["url"]:
+            return result["url"]
+    return "Not Found"
+
 if __name__ == "__main__":
     # revs = extract_reviews_by_person("Aaron")
     # print(revs) # Samsung Galaxy S3, Nikon DSLR
@@ -35,7 +55,7 @@ if __name__ == "__main__":
     system_prompt = (
         "You are a concise analyst.\n"
         "Rules:\n"
-        "1) If asked about a reviewer's opinion, call extract_reviews_by_person\n"
+        "1) If asked about a reviewer's opinion, call extract_reviews_by_person, then find the Amazon link of the product using web_search_product tool\n"
         "Important: After calling tools, response in this format:\n"
         "Sentiment: Positive | Negative | Mixed | Neutral\n"
         "Evidence:\n"
@@ -52,7 +72,7 @@ if __name__ == "__main__":
 
     graph = create_react_agent(
         model=chat_model,
-        tools=[extract_reviews_by_person],
+        tools=[extract_reviews_by_person, web_search_product],
         prompt=system_prompt
     )
 
